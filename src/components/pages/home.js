2441f9cbeaa22108c411ead52b9d49e62801ce37
joeneedum <joeneedum@gmail.com>
2022-01-17 23:52:36 -0500

    commit

---
+++ src/components/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../content/header'
+import '../../stylesheets/home.css'
+
+export default function home() {
+    return (
+        <div>
+            <Header />
+            homepage
+        </div>
+    )
+}
