c2b51ae8221b014968d983cf08008fb4c9682d63
joeneedum <joeneedum@gmail.com>
2022-01-19 17:02:53 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import Home from "./components/pages/home";
 import Single from "./components/pages/single";
 import Write from "./components/pages/write";
 import Setting from './components/pages/setting';
+import Login from "./components/pages/login";
+import Register from "./components/pages/register";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
       <Topbar />
-      <Setting />
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/write' element={<Write/>} />
+        <Route exact path='/post/:postId' element={<Single/>} />
+        <Route exact path='/login' element={<Login/>} />
+        <Route exact path='/register' element={<Register/>} />
+        <Route exact path='/settings' element={<Setting/>} />
+      </Routes>
+    </Router>
     </>
   );
 }
--- src/components/layout/topbar.js
+++ src/components/layout/topbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../stylesheets/topbar.css'
+import { Link } from 'react-router-dom';
 
 export default function topbar() {
     return (
@@ -12,11 +13,24 @@ export default function topbar() {
             </div>
             <div className='topCenter'>
                 <ul className='topList'>
-                    <li className='topListItem'>HOME</li>
-                    <li className='topListItem'>ABOUT</li>
-                    <li className='topListItem'>CONTACT</li>
-                    <li className='topListItem'>WRITE</li>
-                    <li className='topListItem'>LOGOUT</li>
+                    <li className='topListItem'>
+                        <Link to='/'>HOME</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link to='/login'>LOGIN</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link to='/register'>REGISTER</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link to='/settings'>SETTINGS</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link to='/write'>WRITE</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link to='/'>LOGOUT</Link>
+                    </li>
                 </ul>
             </div>
             <div className='topRight'>
